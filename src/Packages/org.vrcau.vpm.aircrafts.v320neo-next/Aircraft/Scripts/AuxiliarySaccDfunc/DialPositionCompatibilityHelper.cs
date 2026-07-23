50a3a685a7e2723c4caf3370714202207b3d6a86
Misaka_L <lipww1234@foxmail.com>
2026-07-23 23:47:43 +0800

    feat: dial positiion event compatibility helper and thrust reverser

---
+++ src/Packages/org.vrcau.vpm.aircrafts.v320neo-next/Aircraft/Scripts/AuxiliarySaccDfunc/DialPositionCompatibilityHelper.cs
@@ -0,0 +1,31 @@
+﻿using SaccFlightAndVehicles;
+using UdonSharp;
+using UnityEngine;
+
+namespace VAU.V320NeoNext.Runtime.AuxiliarySaccDfunc
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class DialPositionCompatibilityHelper : UdonSharpBehaviour
+    {
+        public SaccEntity EntityControl;
+        
+        public void SFEXT_L_EntityStart()
+        {
+            if (!EntityControl)
+            {
+                Debug.LogWarning($"[{nameof(DialPositionCompatibilityHelper)}] EntityControl is null, this script won't do anything.", this);
+                return;
+            }
+
+            foreach (var leftDial in EntityControl.Dial_Functions_L)
+            {
+                leftDial.SendCustomEvent("DFUNC_LeftDial");
+            }
+
+            foreach (var rightDial in EntityControl.Dial_Functions_R)
+            {
+                rightDial.SendCustomEvent("DFUNC_RightDial");
+            }
+        }
+    }
+}
