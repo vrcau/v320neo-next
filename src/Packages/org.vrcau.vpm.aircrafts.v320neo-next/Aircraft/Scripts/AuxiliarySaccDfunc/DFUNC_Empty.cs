a57f0152fcb41df1a95144abf3d8cfae36c560ef
Misaka_L <lipww1234@foxmail.com>
2026-07-26 22:35:33 +0800

    feat: add dial display

---
+++ src/Packages/org.vrcau.vpm.aircrafts.v320neo-next/Aircraft/Scripts/AuxiliarySaccDfunc/DFUNC_Empty.cs
@@ -0,0 +1,9 @@
+using UdonSharp;
+
+namespace VAU.V320NeoNext.Runtime.AuxiliarySaccDfunc
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public sealed class DFUNC_Empty : UdonSharpBehaviour
+    {
+    }
+}
