c55c7b55178152e2820da3ef13396c8d862bfed5
Misaka_L <lipww1234@foxmail.com>
2026-08-14 23:19:59 +0800

    feat: auto setup boarding collider layer

--- src/Packages/org.vrcau.vpm.aircrafts.v320neo-next/EditorScripts/InspectorEditor/VAU320GlobalAircraftSettingsEditor.cs
+++ src/Packages/org.vrcau.vpm.aircrafts.v320neo-next/EditorScripts/InspectorEditor/VAU320GlobalAircraftSettingsEditor.cs
@@ -19,7 +19,32 @@ namespace VAU.V320NeoNext.Editor.InspectorEditor
             if (GUILayout.Button("Setup"))
             {
                 settings.Setup();
+                SetupLayer();
             }
+        }
+
+        // https://github.com/esnya/EsnyaSFAddons/blob/d2984ab062190521d701c9b4651c84abc6e424b9/Packages/com.nekometer.esnya.esnya-sf-addons/Editor/ESFAMenu.cs
+        [MenuItem("SaccFlight/VAU320/Setup EsnyaSFAddons Boarding Collider Layer")]
+        private static void SetupLayer()
+        {
+            SetLayerName(29, "BoardingCollider");
+            for (var i = 0; i < 32; i++)
+            {
+                Physics.IgnoreLayerCollision(29, i, ((1 << i) & 0b0101_1111_1111_1101_1010_1111_1101_1111) == 0);
+            }
+        }
+
+        private static void SetLayerName(int layer, string name)
+        {
+            var tagManager =
+                new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset"));
+            tagManager.Update();
+
+            var layersProperty = tagManager.FindProperty("layers");
+            layersProperty.arraySize = Mathf.Max(layersProperty.arraySize, layer);
+            layersProperty.GetArrayElementAtIndex(layer).stringValue = name;
+
+            tagManager.ApplyModifiedProperties();
         }
     }
 }
